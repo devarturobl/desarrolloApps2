c82f79791661190f24d9d0abf33e6bc6397af44c
devarturobl <4rthurbl00@gmail.com>
2025-02-05 08:32:26 -0600

    container

---
+++ Semana 2/navigator/lib/pages/mycontainer.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class Mycontainer extends StatefulWidget {
+  const Mycontainer({super.key});
+
+  @override
+  State<Mycontainer> createState() => _MycontainerState();
+}
+
+class _MycontainerState extends State<Mycontainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 350,
+              width: 350,
+              decoration: BoxDecoration(
+                  color: Colors.black, borderRadius: BorderRadius.circular(30)),
+              child: Center(
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.red,
+                        width: 5.0,
+                      )),
+                  child: Center(
+                    child: Text(
+                      "Hola",
+                      style: TextStyle(color: Colors.white, fontSize: 40.0),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 350,
+              width: 350,
+              color: Colors.black,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.red,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.blue,
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
