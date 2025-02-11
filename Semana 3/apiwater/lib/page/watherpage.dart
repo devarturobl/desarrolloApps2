c06389b655b8b7f0a48ab4e8e121ec64f78dd4e0
devarturobl <4rthurbl00@gmail.com>
2025-02-11 10:01:12 -0600

    openWather3

--- Semana 3/apiwater/lib/page/watherpage.dart
+++ Semana 3/apiwater/lib/page/watherpage.dart
@@ -1,3 +1,5 @@
+import 'package:apiwater/model/wather_model.dart';
+import 'package:apiwater/services/wather_service.dart';
 import 'package:flutter/material.dart';
 
 class Watherpage extends StatefulWidget {
@@ -8,6 +10,26 @@ class Watherpage extends StatefulWidget {
 }
 
 class _WatherpageState extends State<Watherpage> {
+  // api key
+  final _watherService = WatherService('2d6f1db34d02373c1ce9c7e5a07ffcc0');
+  Wather? _wather;
+
+  // get the current city
+  _fetchWather() async {
+    String cityName = await _watherService.getCurrentCity();
+
+    // get the wather city
+
+    try {
+      final water = await _watherService.getWather(cityName);
+      setState(() {
+        _wather = water;
+      });
+    } 
+    catch (e) {
+      print(e);
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Container();
