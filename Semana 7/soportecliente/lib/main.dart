7d45c88ab7ec2e45d6a90b8d6d94485110c37a8e
devarturobl <4rthurbl00@gmail.com>
2025-03-10 10:21:21 -0600

    semana7 supabase

---
+++ Semana 7/soportecliente/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async{
+  Supabase.initialize(
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJuZmd0bmZ1eWNxb3ZwemphdHlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE2MjAzMjUsImV4cCI6MjA1NzE5NjMyNX0.ehyrrrHEmUtn5Zn2j4FBw7ee16Dt7fpT5TAMH-7lmpo",  
+    url: "https://bnfgtnfuycqovpzjatyd.supabase.co",
+  );  
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
